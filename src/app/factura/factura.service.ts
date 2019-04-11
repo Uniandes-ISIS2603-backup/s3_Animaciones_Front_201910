af3d9a3e0e9c984e4fcdf9fc767213616b237494
na.alvarado <na.alvarado@uniandes.edu.co>
2019-04-11 09:00:47 -0500

    Arreglo factura

--- src/app/factura/factura.service.ts
+++ src/app/factura/factura.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 
-import {Factura} from './Factura';
+import {Factura} from './factura';
 
 @Injectable()
 export class FacturaService {
