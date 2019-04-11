449152ef644f2ca2f4f1b5c538b59282ced98816
Daniel <df.serrano@uniandes.edu.co>
2019-04-11 07:48:48 -0500

    Ronda list added

--- src/app/ronda/ronda-list/ronda-list.component.ts
+++ src/app/ronda/ronda-list/ronda-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { Ronda } from '../ronda';
-import { RondaService } from '../ronda.service';
+import { RondaService} from '../ronda.service';
+
+
 @Component({
   selector: 'app-ronda-list',
   templateUrl: './ronda-list.component.html',
@@ -10,13 +11,23 @@ import { RondaService } from '../ronda.service';
 })
 export class RondaListComponent implements OnInit {
 
-  constructor(private rondaService: RondaService) { }
-  rondas: Ronda[]
-  getRondas(): void {
-    this.rondaService.getRondas().subscribe(rondas => {
-        this.rondas = rondas;
-    });
-}
+constructor(private RondasService: RondaService, private router: Router) { }
+
+rondas: Ronda[];
+Rondas_id: number;
+selectedRonda: Ronda;
+
+getRondas(): void {
+    this.RondasService.getRondas().subscribe(Rondas => this.rondas = Rondas);
+  }
+
+  onSelected(Rondas_id: number): void {
+    this.Rondas_id = Rondas_id;
+    this.selectedRonda = new Ronda();
+    this.RondasService.getRondaDetail(Rondas_id).subscribe(o => this.selectedRonda = o);
+  }
+
+
 
   ngOnInit() {
     this.getRondas();
