44f3e4f7eadfb304c57e8d2ecaf975558229a102
Daniel <df.serrano@uniandes.edu.co>
2019-04-11 07:49:42 -0500

    Ronda detail added

--- src/app/ronda/ronda-detail/ronda-detail.component.ts
+++ src/app/ronda/ronda-detail/ronda-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RondaService } from '../ronda.service';
+import { Ronda } from '../ronda';
+import { RondaDetail } from '../ronda-detail';
+import {Votacion} from '../../votacion/votacion'
 @Component({
   selector: 'app-ronda-detail',
   templateUrl: './ronda-detail.component.html',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RondaDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private rondaService: RondaService,
+    private route: ActivatedRoute) { 
+  }
+@Input() rondaD: RondaDetail
+rondas_id : number;
+
 
   ngOnInit() {
+    this.rondas_id = +this.route.snapshot.paramMap.get('id');
+    if (this.rondas_id) {
+      this.rondaD = new RondaDetail();
+      this.getRondaDetail();
+      console.log(this.rondaD);
+    }
   }
 
+  getRondaDetail(): void {
+    this.rondaService.getRondaDetail(this.rondas_id)
+      .subscribe(ronda => {
+        this.rondaD= ronda
+      });
+  }
+  totalPuntos() : number{
+    var total  = 0;
+    for(let e of  this.rondaD.votaciones )
+    {
+      total += e.puntos;
+    }
+    return total;
+    
+  }
 }
