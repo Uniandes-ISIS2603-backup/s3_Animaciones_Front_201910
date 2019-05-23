abe75bbfbaa88b5b0df0dcf9ce4762c868cc454c
Luis José García <lj.garciam1@uniandes.edu.co>
2019-05-22 22:49:14 -0500

    Hmmmm

--- src/app/propuesta/propuesta-edit/propuesta-edit.component.ts
+++ src/app/propuesta/propuesta-edit/propuesta-edit.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import {Router} from '@angular/router';
+import {PropuestaService} from '../propuesta.service';
+import {ToastrService} from 'ngx-toastr';
+import {Propuesta} from '../propuesta';
+import {Factura} from '../../factura/factura';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-propuesta-edit',
   templateUrl: './propuesta-edit.component.html',
   styleUrls: ['./propuesta-edit.component.css']
 })
-export class PropuestaEditComponent implements OnInit {
+export class PropuestaEditComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  constructor(private service: PropuestaService, private toastr: ToastrService, private route: ActivatedRoute, private router:Router) { }
+  
+  //@Input() propuesta: Propuesta;
+  
+  @Output() cancel = new EventEmitter();
+
+  @Output() update = new EventEmitter();
+  
+  id: number;
+  
+  @Input() factura: Factura;
 
   ngOnInit() {
+      this.id = +this.route.snapshot.paramMap.get('id');
+      this.factura = new Factura();
+  }
+  
+  ngOnChanges(){
+        this.ngOnInit()
+  }
+  
+  cancelEdition(): void {
+        this.cancel.emit();
+  }
+  
+  newFactura():void{
+      this.service.updatePropuesta(this.factura, this.id).subscribe(yo =>{
+          this.toastr.success("Se creó la factura","yo");
+          this.router.navigate(['/propuestas']);
+      });
+      this.update.emit();
   }
 
 }
--- src/app/propuesta/propuesta.service.ts
+++ src/app/propuesta/propuesta.service.ts
@@ -13,20 +13,39 @@ const prop = 'propuestas';
     providedIn: 'root'
 })
 export class PropuestaService {
-
+    
+    /**
+     * Constructor de la clase
+     */
   constructor(private http: HttpClient) { }
-
+    
+    /**
+     * Método que retorna todas las propuestas
+     */
   getPropuestas(): Observable<Propuesta[]>{
     return this.http.get<Propuesta[]>(API_URL+prop);
   }
-
+    
+    /**
+     * Método que retorna una propuesta específica
+     */
   getPropuestaDetail(id): Observable<Propuesta>{
     return this.http.get<Propuesta>(API_URL+prop+"/"+id);
   }
 
+    /**
+     * Método que crea una nueva propuesta
+     */
   createPropuesta(yo): Observable<Propuesta>{
      // this.http.post<Factura>('');
     return this.http.post<Propuesta>(API_URL + prop,yo);
   }
+  
+    /**
+     * Método que actualiza una propuesta con una nueva FACTURA
+     */
+  updatePropuesta(factura, yo): Observable<Propuesta> {
+     return this.http.put<Propuesta>(API_URL + prop + '/' + yo+'/factura', factura);
+   }
 
 }
